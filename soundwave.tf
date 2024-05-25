e0070c14e683d6d3c4bc4d3f4c208890a9d52085
Willardth <162396594+Willardth@users.noreply.github.com>
2024-05-25 12:50:20 -0700

    Add files via upload

--- soundwave.tf
+++ soundwave.tf
@@ -13,3 +13,26 @@ provider "google" {
   region      = "us-central1"
   
 }
+
+ variable "secret" {
+  default = "secret"
+  description = "This is a secret key"
+ }
+ resource "google_compute_instance" "vm" {
+  name         = "test-instance"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+   
+ }
+ }
